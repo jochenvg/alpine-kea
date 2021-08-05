3293efc9ea3ad10b63fd0f12b6c2116b7bf3d30c
jochenvg <jochen@vanguyse.net>
2021-08-05 11:20:36 +0100

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-set -e
-
 case $1 in
 	init)
 		kea-admin db-init mysql -h $KEA_ADMIN_DB_HOST -u $KEA_ADMIN_DB_USER -n $KEA_ADMIN_DB_NAME || \
 		kea-admin db-upgrade mysql -h $KEA_ADMIN_DB_HOST -u $KEA_ADMIN_DB_USER -n $KEA_ADMIN_DB_NAME
+		;;
+	dhcp4)
+		kea-dhcp4 -d -c /etc/kea/kea-dhcp4.conf
 		;;
 	*)
 		exec "$@"
