101e2e230464af20b83ad0f976e8e28da9393dce
jochenvg <jochen@vanguyse.net>
2021-08-05 04:24:22 +0100

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 case $1 in
 	init)
-		kea-admin db-init mysql -h $KEA_ADMIN_DB_HOST -i $KEA_ADMIN_DB_USER -n $KEA_ADMIN_DB_NAME || \
-		kea-admin db-upgrade mysql -h $KEA_ADMIN_DB_HOST -i $KEA_ADMIN_DB_USER -n $KEA_ADMIN_DB_NAME
+		kea-admin db-init mysql -h $KEA_ADMIN_DB_HOST -u $KEA_ADMIN_DB_USER -n $KEA_ADMIN_DB_NAME || \
+		kea-admin db-upgrade mysql -h $KEA_ADMIN_DB_HOST -u $KEA_ADMIN_DB_USER -n $KEA_ADMIN_DB_NAME
 		;;
 	*)
 		exec "$@"
